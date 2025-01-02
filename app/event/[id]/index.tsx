4cafd9df784f4f51be1757e8af1b42ffc074dc75
Luis Leal <luisrrleal@gmail.com>
2025-01-01 21:31:12 -0600

    Add new view

--- app/event/[id]/index.tsx
+++ app/event/[id]/index.tsx
@@ -1,168 +1,189 @@
 import React, { useState } from 'react';
-import { View, Text, ImageBackground, StyleSheet, ScrollView, TextInput, Image } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, TextInput, Image, TouchableOpacity, FlatList } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 import styles from '@/constants/Styles';
 import Button from '@/components/ui/Button';
-
+import { Link, useRouter } from 'expo-router';
 import * as ImagePicker from 'expo-image-picker';
 import PinterestGrid from '../../../components/ui/PinterestGrid';
 
-
 const EventView = () => {
-    const [isRegistered, setIsRegistered] = useState(false);
-    const [eventStarted, setEventStarted] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [eventStarted, setEventStarted] = useState(false);
+  const [code, setCode] = useState('');
+  const router = useRouter();
 
-    const [code, setCode] = useState('');
+  const handleRegister = () => {
+    setIsRegistered(true);
+  };
 
-    const handleRegister = () => {
-        setIsRegistered(true);
-    };
+  const handleAddMemory = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      quality: 0.5,
+    });
 
-    const handleAddMemory = async () => {
-        let result = await ImagePicker.launchCameraAsync({
-            allowsEditing: true,
-            quality: 0.5,
-        });
+    if (!result.canceled) {
+      // Lógica para manejar la imagen tomada
+    }
+  };
 
-        if (!result.canceled) {
-            // Lógica para manejar la imagen tomada
+  const renderHeader = () => (
+    <View>
+      <ImageBackground
+        source={{ uri: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg' }}
+        style={{ width: '100%', height: 500, display: 'flex', justifyContent: 'center', alignSelf: 'center', alignItems: 'center' }}
+      >
+        <LinearGradient
+          colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.7)']}
+          style={localStyles.gradient}
+        />
+        <View>
+          <Text style={[styles.white, styles.bold, { fontSize: 60 }]}>NEON PARTY</Text>
+          <View style={{ marginTop: 40 }}>
+            <Text style={[styles.white, styles.centerText, styles.fontSize24, styles.bold]}>340</Text>
+            <Text style={[styles.white, styles.centerText, styles.fontSize20, styles.bold]}>asistentes</Text>
+          </View>
+        </View>
+      </ImageBackground>
+
+      <View style={localStyles.infoContainer}>
+        <View style={[styles.flex, styles.justifyBetween, styles.row]}>
+          <View style={localStyles.infoRow}>
+            <Ionicons name="calendar" size={24} color={styles.primaryColor.color} />
+            <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>14 Dic 2025</Text>
+          </View>
+          <View style={localStyles.infoRow}>
+            <Ionicons name="time" size={24} color={styles.primaryColor.color} />
+            <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>8:00 PM</Text>
+          </View>
+        </View>
+        <Text style={[styles.white, styles.marginTop10, styles.fontSize16, styles.marginBottom20, styles.justifyText]}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut asperiores accusamus hic consectetur, ab reprehenderit. Temporibus voluptatem, nesciunt excepturi sit vero, possimus a maxime laboriosam sapiente, perspiciatis ipsam cupiditate sunt!
+        </Text>
+        <View style={localStyles.infoRow}>
+          <Ionicons name="location" size={24} color={styles.primaryColor.color} />
+          <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>Casa Rotaria</Text>
+        </View>
+        <View style={localStyles.infoRow}>
+          <Ionicons name="shirt" size={24} color={styles.primaryColor.color} />
+          <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>Neon colors</Text>
+        </View>
+        <View style={localStyles.servicesContainer}>
+          <FontAwesome5 name="shield-alt" size={24} color={styles.primaryColor.color} />
+          <FontAwesome5 name="beer" size={24} color={styles.primaryColor.color} />
+          <FontAwesome5 name="smoking" size={24} color={styles.primaryColor.color} />
+          <FontAwesome5 name="ambulance" size={24} color={styles.primaryColor.color} />
+          <FontAwesome5 name="utensils" size={24} color={styles.primaryColor.color} />
+          <FontAwesome5 name="swimmer" size={24} color={styles.primaryColor.color} />
+        </View>
+      </View>
+
+      {!isRegistered && !eventStarted && (
+        <View style={localStyles.registrationContainer}>
+          <TextInput
+            style={[styles.input, styles.white, styles.marginBottom10]}
+            placeholder="Ingresa tu código"
+            placeholderTextColor="#888"
+            value={code}
+            onChangeText={setCode}
+          />
+          <Button title="Registrarse" onPress={handleRegister} />
+        </View>
+      )}
+
+      {isRegistered && !eventStarted && (
+        <View style={localStyles.qrContainer}>
+          <Image source={{ uri: 'https://luisrrleal.com/_next/image?url=https%3A%2F%2Fquickchart.io%2Fqr%3Ftext%3Dhttps%3A%2F%2Fluisrrleal.com%2F%26width%3D300%26height%3D300%26format%3Dpng&w=640&q=75' }} style={localStyles.qrCode} />
+          <Text style={[styles.white, styles.marginBottom10, styles.marginTop50]}>Ya estás registrado. Presenta este código QR en la entrada.</Text>
+        </View>
+      )}
+
+      {eventStarted && (
+        <View style={localStyles.memoryContainer}>
+          <Button title="Agregar Memoria" onPress={handleAddMemory} />
+          <Text style={[styles.white, styles.marginBottom10, styles.marginTop50]}>
+            Las memorias son imágenes que se compartirán con todos los asistentes de la fiesta 24 horas después de haberse terminado.
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+
+  return (
+    <View style={styles.flex1}>
+      <FlatList
+        data={[]}
+        ListHeaderComponent={renderHeader}
+        ListFooterComponent={
+          <PinterestGrid images={
+            [
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+              'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
+            ]
+          } />
         }
-    };
-
-    return (
-        <ScrollView style={[styles.flex1, styles.blackBg]}>
-            <ImageBackground
-                source={{ uri: 'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg' }}
-                // style={localStyles.imageBackground}
-                style={{ width: '100%', height: 500, display: 'flex', justifyContent: 'center', alignSelf: 'center', alignItems: 'center' }}
-            >
-                <LinearGradient
-                    colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.7)']}
-                    style={localStyles.gradient}
-                />
-                <View>
-                    <Text style={[styles.white, styles.bold, { fontSize: 60 }]}>NEON PARTY</Text>
-                    <View style={{ marginTop: 40 }}>
-                        <Text style={[styles.white, styles.centerText, styles.fontSize24, styles.bold]}>340</Text>
-                        <Text style={[styles.white, styles.centerText, styles.fontSize20, styles.bold]}>asistentes</Text>
-                    </View>
-                </View>
-                {/* <View style={{}}>
-                    <View style={localStyles.ribbon}>
-                        <Text style={[styles.white, styles.bold]}>14 Dic</Text>
-                    </View>
-                </View> */}
-            </ImageBackground>
-
-            <View style={localStyles.infoContainer}>
-                <View style={[styles.flex, styles.justifyBetween, styles.row]}>
-                    <View style={localStyles.infoRow}>
-                        <Ionicons name="calendar" size={24} color={styles.primaryColor.color} />
-                        <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>14 Dic 2025</Text>
-                    </View>
-                    <View style={localStyles.infoRow}>
-                        <Ionicons name="time" size={24} color={styles.primaryColor.color} />
-                        <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>8:00 PM</Text>
-                    </View>
-                </View>
-                <Text style={[styles.white, styles.marginTop10, styles.fontSize16, styles.marginBottom20, styles.justifyText]}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut asperiores accusamus hic consectetur, ab reprehenderit. Temporibus voluptatem, nesciunt excepturi sit vero, possimus a maxime laboriosam sapiente, perspiciatis ipsam cupiditate sunt!
-                </Text>
-                <View style={localStyles.infoRow}>
-                    <Ionicons name="location" size={24} color={styles.primaryColor.color} />
-                    <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>Casa Rotaria</Text>
-                </View>
-                <View style={localStyles.infoRow}>
-                    <Ionicons name="shirt" size={24} color={styles.primaryColor.color} />
-                    <Text style={[styles.white, styles.marginLeft10, styles.fontSize16]}>Neon colors</Text>
-                </View>
-                <View style={localStyles.servicesContainer}>
-                    <FontAwesome5 name="shield-alt" size={24} color={styles.primaryColor.color} />
-                    <FontAwesome5 name="beer" size={24} color={styles.primaryColor.color} />
-                    <FontAwesome5 name="smoking" size={24} color={styles.primaryColor.color} />
-                    <FontAwesome5 name="ambulance" size={24} color={styles.primaryColor.color} />
-                    <FontAwesome5 name="utensils" size={24} color={styles.primaryColor.color} />
-                    <FontAwesome5 name="swimmer" size={24} color={styles.primaryColor.color} />
-                </View>
-            </View>
-
-            {!isRegistered && !eventStarted && (
-                <View style={localStyles.registrationContainer}>
-                    <TextInput
-                        style={[styles.input, styles.white, styles.marginBottom10]}
-                        placeholder="Ingresa tu código"
-                        placeholderTextColor="#888"
-                        value={code}
-                        onChangeText={setCode}
-                    />
-                    <Button title="Registrarse" onPress={handleRegister} />
-                </View>
-            )}
-
-            {isRegistered && !eventStarted && (
-                <View style={localStyles.qrContainer}>
-                    <Image source={{ uri: 'https://luisrrleal.com/_next/image?url=https%3A%2F%2Fquickchart.io%2Fqr%3Ftext%3Dhttps%3A%2F%2Fluisrrleal.com%2F%26width%3D300%26height%3D300%26format%3Dpng&w=640&q=75' }} style={localStyles.qrCode} />
-                    <Text style={[styles.white, styles.marginBottom10, styles.marginTop50]}>Ya estás registrado. Presenta este código QR en la entrada.</Text>
-                </View>
-            )}
-
-            {eventStarted && (
-                <View style={localStyles.memoryContainer}>
-                    <Button title="Agregar Memoria" onPress={handleAddMemory} />
-                    <Text style={[styles.white, styles.marginBottom10, styles.marginTop50]}>
-                        Las memorias son imágenes que se compartirán con todos los asistentes de la fiesta 24 horas después de haberse terminado.
-                    </Text>
-                </View>
-            )}
-
-            {/* Pinterest grid for pictures */}
-            <PinterestGrid images={
-                [
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                    'https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img/https://www.duendesproducciones.com/wp-content/uploads/2022/04/uv-party-2.jpg',
-                ]} />
-        </ScrollView>
-    );
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={() => null}
+      />
+      <Link
+        style={localStyles.floatingButton}
+        // onPress={() => router.push('/event/qr-scanner')}
+        href="/event/qr-scanner"
+      >
+        <Ionicons name="qr-code" size={30} color="white" />
+      </Link>
+    </View>
+  );
 };
 
 const localStyles = StyleSheet.create({
-    gradient: {
-        ...StyleSheet.absoluteFillObject,
-        height: '100%',
-    },
-    infoContainer: {
-        padding: 20,
-    },
-    infoRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    servicesContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 20,
-    },
-    registrationContainer: {
-        marginBottom: 50,
-        padding: 20,
-    },
-    qrContainer: {
-        padding: 20,
-        alignItems: 'center',
-    },
-    qrCode: {
-        width: 200,
-        height: 200,
-    },
-    memoryContainer: {
-        padding: 20,
-    },
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+  },
+  infoContainer: {
+    padding: 20,
+  },
+  infoRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  servicesContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+  registrationContainer: {
+    marginBottom: 50,
+    padding: 20,
+  },
+  qrContainer: {
+    padding: 20,
+    alignItems: 'center',
+  },
+  qrCode: {
+    width: 200,
+    height: 200,
+  },
+  memoryContainer: {
+    padding: 20,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#4D4DFF',
+    borderRadius: 50,
+    padding: 15,
+    elevation: 5,
+  },
 });
 
 export default EventView;
---
+++ app/event/qr-scanner/index.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Camera, CameraView } from 'expo-camera';
+import { useRouter } from 'expo-router';
+import styles from '@/constants/Styles';
+
+const QRScanner = () => {
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+    const [scanned, setScanned] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }: { type: string; data: string }) => {
+        setScanned(true);
+        alert(`Código QR escaneado: ${data}`);
+        // Aquí puedes manejar el código escaneado, por ejemplo, redirigir a otra página
+    };
+
+    if (hasPermission === null) {
+        return <Text>Solicitando permiso para usar la cámara...</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No se ha concedido permiso para usar la cámara.</Text>;
+    }
+
+    return (
+        <View style={styles.flex1}>
+            <CameraView barcodeScannerSettings={{
+                barcodeTypes: ["qr"],
+            }} />
+        </View>
+    );
+};
+
+const localStyles = StyleSheet.create({
+    rescanButton: {
+        position: 'absolute',
+        bottom: 50,
+        left: '50%',
+        marginLeft: -30,
+        backgroundColor: '#4D4DFF',
+        borderRadius: 50,
+        padding: 15,
+        elevation: 5,
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    topOverlay: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    centerOverlay: {
+        flexDirection: 'row',
+    },
+    leftOverlay: {
+        flex: 1,
+        height: 200,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    focused: {
+        width: 200,
+        height: 200,
+        borderColor: '#4D4DFF',
+        borderWidth: 2,
+    },
+    rightOverlay: {
+        flex: 1,
+        height: 200,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    bottomOverlay: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+});
+
+export default QRScanner;
